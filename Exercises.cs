49f9eb9763c71bb67ceb80600e6ef020464c2057
Ryan Cornett <ryandcornett@gmail.com>
2023-08-03 14:33:00 -0400

    Added Exercises 4 & 5

--- Exercises.cs
+++ Exercises.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Mosh.ListExercises
 {
@@ -34,7 +30,8 @@ namespace Mosh.ListExercises
                 userInput = Console.ReadLine();
             }
         }
-                public void Exercise2()
+
+        public void Exercise2()
         {
             Console.Write("Enter your name: ");
             string yourName = Console.ReadLine();
@@ -48,6 +45,109 @@ namespace Mosh.ListExercises
             string reversedName = new string(arr);
             Console.WriteLine(reversedName);
         }
-		
+
+        public void Exercise3()
+        {
+            List<int> numberList = new();
+            bool isUnique = true;
+            bool checkNumberList(int input)
+            {
+                foreach (int number in numberList)
+                {
+                    if (number == input)
+                    {
+                        isUnique = false;
+                    }
+                }
+                return isUnique;
+            }
+
+            Console.WriteLine("**************************\n* ENTER 5 UNIQUE NUMBERS *\n**************************");
+            Console.Write("Enter an integer: ");
+            int numberInput = Int32.Parse(Console.ReadLine());
+            numberList.Add(numberInput);
+            while (numberList.Count < 5)
+            {
+                Console.Write("Enter another integer: ");
+                numberInput = Int32.Parse(Console.ReadLine());
+                if (checkNumberList(numberInput))
+                {
+                    numberList.Add(numberInput);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                    isUnique = true;
+                }
+            }
+            numberList.Sort();
+            Console.WriteLine($"Your list of five unique numbers is: ");
+            foreach (int number in numberList)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+
+        public void Exercise4()
+        {
+            List<int> numbersEntered = new List<int>();
+            int userInt = 0;
+            Console.WriteLine("Enter a number -OR- 'Quit' to exit");
+            string userInput = Console.ReadLine();
+            while (userInput != "Quit") 
+            {
+                userInt = Int32.Parse(userInput);
+                if (!numbersEntered.Contains(userInt))
+                {
+                    numbersEntered.Add(userInt);
+                    numbersEntered.Sort();
+                }
+                Console.WriteLine("You have entered " + ((numbersEntered.Count > 1) ? "these numbers:" : "this number:"));
+                foreach (int number in numbersEntered) 
+                {
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Enter another number (or 'Quit') : ");
+                userInput = Console.ReadLine();
+            }
+
+        }
+
+        public void Exercise5()
+        {
+            List<int> numbersEntered = new List<int>();
+            bool longEnough = false;
+            void CheckLength()
+            {
+                if (numbersEntered.Count >= 5)
+                {
+                    longEnough = true;
+                }
+            }
+            
+            Console.WriteLine("Supply a list of comma + space separated numbers (e.g 5, 1, 9, 2, 10):");
+            string userInput = Console.ReadLine();
+            foreach (var sub in userInput.Split(", "))
+            {
+                int num;
+                if (int.TryParse(sub, out num))
+                    numbersEntered.Add(num);
+                CheckLength();
+            }
+
+            if (!longEnough)
+            {
+                Console.WriteLine("Invalid List");
+                numbersEntered.Clear();
+                Exercise5();
+            }
+            else
+            {
+                numbersEntered.Sort();
+                Console.WriteLine($"The three smallest numbers you entered are: {numbersEntered[0]}, {numbersEntered[1]}, and {numbersEntered[2]}.");
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,58 @@
 ﻿using Mosh.ListExercises;
 
 Exercises exercises = new Exercises();
-exercises.Exercise1();
+
+void InvalidEntry()
+{
+    Console.Clear();
+    Console.WriteLine("Invalid entry. Try again.");
+    Console.WriteLine();
+    ShowMenu();
+}
+void ShowMenu()
+{
+    int userSelection = -1;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("****************************\n*     CHOOSE A FUNCTION    *\n* 1. Facebook Likes Display\n* 2. Name Reverser\n* 3. Five Unique Numbers\n* 4. List of Unique Numbers\n* 5. Comma Separated Numbers\n* 0. Exit the Application\n****************************");
+    Console.ForegroundColor = ConsoleColor.White;
+    string serialUserSelection = Console.ReadLine();
+    try
+    {
+        userSelection = Int32.Parse(serialUserSelection);
+    }
+    catch (Exception ex)
+    {
+        InvalidEntry();
+    }
+
+    switch (userSelection)
+    {
+        case 0:
+            Environment.Exit(0); break;
+        
+        case 1:
+            exercises.Exercise1(); break;
+
+        case 2:
+            exercises.Exercise2(); break;
+        
+        case 3:
+            exercises.Exercise3(); break;
+
+        case 4:
+            exercises.Exercise4(); break;
+
+        case 5:
+            exercises.Exercise5();
+            break;
+
+        default:
+            InvalidEntry();
+            break;
+    }
+}
+
+ShowMenu();
 
 
 
